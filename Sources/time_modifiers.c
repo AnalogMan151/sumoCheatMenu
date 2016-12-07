44578210a5fb23f7cd23c72a4275b2df8f8c3525
AnalogMan151 <james.p.wolfe@outlook.com>
2016-12-06 19:19:37 -0500

    Code cleanup

--- Sources/time_modifiers.c
+++ Sources/time_modifiers.c
@@ -18,28 +18,29 @@
      timeOffset = timeOffset / 3600;
 
      if (timeOffset == 24) {
-        strcpy(timeType, "Sun ");
+        strcpy(timeType, "Sun");
         timeHours = 0;
      } else if (timeOffset < 12) {
-        strcpy(timeType, "Sun ");
+        strcpy(timeType, "Sun");
         timeHours = timeOffset;
      } else if (timeOffset >= 12) {
-        strcpy(timeType, "Moon ");
+        strcpy(timeType, "Moon");
         timeHours = timeOffset - 12;
      }
 
-     xsprintf(currentTime, "Current: %s Time +%d hours", timeType, timeHours);
+     xsprintf(currentTime, "Current: %4s Time +%-2d hours", timeType, timeHours);
  }
 
+
+// Increases time offset by one hour and rolls over if at max
  void   increaseTime(void) {
     u32 timeOffset = READU32(0x330D9238);
     timeOffset = timeOffset / 3600;
 
-    if (timeOffset == 24) {
+    if (timeOffset == 24)
         timeOffset = 1;
-    } else {
+    else
         timeOffset += 1;
-    }
 
     timeOffset = timeOffset * 3600;
 
@@ -48,15 +49,16 @@
     disableCheat(i_increaseTime);
  }
 
+
+// Decreases time offset by one hour and rolls over if at min
  void   decreaseTime(void) {
     u32 timeOffset = READU32(0x330D9238);
     timeOffset = timeOffset / 3600;
 
-    if (timeOffset == 1) {
+    if (timeOffset == 1)
         timeOffset = 24;
-    } else {
+    else
         timeOffset -= 1;
-    }
 
     timeOffset = timeOffset * 3600;
 
